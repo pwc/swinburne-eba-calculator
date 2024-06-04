80becec3a6513d3905a63f3a9e4d6f08ad5d3f39
Patrick Clearwater <patrick@clearwater.id.au>
2024-06-04 20:51:31 +1000

    Skeleton

--- swinburne-eba-calculator/src/App.tsx
+++ swinburne-eba-calculator/src/App.tsx
@@ -2,24 +2,43 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function TableOfValues(props: {}) {
+	return <table>
+		<thead>
+			<tr>
+				<th>Date</th>
+				<th>Nominal pay</th>
+				<th>CPI</th>
+				<th>Deflator</th>
+				<th>Pay in reference dollars</th>
+				<th>Comment</th>
+			</tr>
+		</thead>
+		<tbody>
+		</tbody>
+	</table>;
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<main>
+	<h1>Swinburne EBA calculator</h1>
+	<p>This application tells you how you fare given Swinburne’s pay rises.</p>
+	<p>Note that it <b>doesn’t</b> take into account promotions or increases in increment (merit pay rises). These reflect your improved skills, performance and experience, rather than Swinburne increasing your salary.</p>
+	<h2>Settings</h2>
+	<label>Start date: <input type="date" /></label>
+	<label>CPI group: <select>
+		<option value="melbourne">Melbourne</option>
+		<option value="australia">Australia-wide</option>
+		<option value="brisbane">Brisbane</option>
+	</select></label>
+	<label>Pay grade: <select>
+		<option value="index">Index ($100)</option>
+		<option value="hew7">HEW 7</option>
+		<option value="hew8">HEW 8</option>
+	</select></label>
+	<h2>Table of values</h2>
+	<TableOfValues />
+	</main>
   );
 }
 
